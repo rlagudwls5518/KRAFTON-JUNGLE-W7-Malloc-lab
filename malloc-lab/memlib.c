b4fb33e654bfb21f8b156eae9e4d92a0b14d690c
rlagudwls5518 <rlagudwls5518@naver.com>
2025-08-23 10:26:08 +0900

    08/23

--- malloc-lab/memlib.c
+++ malloc-lab/memlib.c
@@ -15,16 +15,22 @@
 #include "config.h"
 
 /* private variables */
-static char *mem_start_brk;  /* points to first byte of heap */
-static char *mem_brk;        /* points to last byte of heap */
-static char *mem_max_addr;   /* largest legal heap address */ 
+static char *mem_start_brk;  /* points to first byte of heap, 힙의 첫 바이트를 가리키는 변수 */
+static char *mem_brk;        /* points to last byte of heap, 힙의 마지막 바이트를 가리키는 변수. 여기에다가 sbrk 함수로 새로운 사이지의 힙을 할당받는다.  */
+static char *mem_max_addr;   /* largest legal heap address, 힙의 최대 크기(20MB)의 주소를 가리키는 변수 */ 
 
 /* 
  * mem_init - initialize the memory system model
+     최대 힙 메모리 공간을 할당받고 초기화해준다. 
  */
 void mem_init(void)
 {
     /* allocate the storage we will use to model the available VM */
+    // config.h에 정의되어 있음, #define MAX_HEAP (20*(1<<20)) : 20971520bytes == 20 MB
+    // 먼저 20MB만큼의 MAX_HEAP을 malloc으로 동적할당해온다. 만약 메모리를 불러오는데 실패했다면 에러 메세지를 띄우고 프로그램을 종료한다.
+    // 그 시작점을 mem_start_brk라 한다.
+    // 아직 힙이 비어 있으므로 mem_brk도 mem_start_brk와 같다.
+
     if ((mem_start_brk = (char *)malloc(MAX_HEAP)) == NULL) {
 	fprintf(stderr, "mem_init_vm: malloc error\n");
 	exit(1);
@@ -37,6 +43,7 @@ void mem_init(void)
 /* 
  * mem_deinit - free the storage used by the memory system model
  */
+// init으로 초기 할당했던 힙 전체 다시 반납
 void mem_deinit(void)
 {
     free(mem_start_brk);
@@ -45,6 +52,7 @@ void mem_deinit(void)
 /*
  * mem_reset_brk - reset the simulated brk pointer to make an empty heap
  */
+// 시뮬레이션 된 힙을 완전히 비어있는 상태로 되돌림 포인터 초기화
 void mem_reset_brk()
 {
     mem_brk = mem_start_brk;
@@ -55,15 +63,24 @@ void mem_reset_brk()
  *    by incr bytes and returns the start address of the new area. In
  *    this model, the heap cannot be shrunk.
  */
+
+ // byte 단위로 필요 메모리 크기를 입력받아 그 크기만큼 힙을 늘려주고, 새 메모리의 시작 지점을 리턴한다.
+ // 힙을 줄이려 하거나 최대 힙 크기를 넘어버리면 리턴한다.
+ // old_brk의 주소를 리턴하는 이유: 새로 늘어난 힙의 첫번째 주소이기 때문
+// 리턴 할때 왜 형변환을 하는지?? -> 그냥 정수면 정수 캐릭터면 캐릭터를 쓰며냐 되는디
+// --> 함수가 매개변수로 받아들여야  하는 포인터가 어떤 자료형에 대한 포인터인지 알 수가 없을수 있기 때문에 
+// (void는 모든 종류의 자료형을 받을 수 있다.)
+
 void *mem_sbrk(int incr) 
 {
     char *old_brk = mem_brk;
 
     if ( (incr < 0) || ((mem_brk + incr) > mem_max_addr)) {
-	errno = ENOMEM;
-	fprintf(stderr, "ERROR: mem_sbrk failed. Ran out of memory...\n");
-	return (void *)-1;
+        errno = ENOMEM;
+        fprintf(stderr, "ERROR: mem_sbrk failed. Ran out of memory...\n");
+        return (void *)-1;
     }
+
     mem_brk += incr;
     return (void *)old_brk;
 }
@@ -71,6 +88,7 @@ void *mem_sbrk(int incr)
 /*
  * mem_heap_lo - return address of the first heap byte
  */
+//힙의 첫번째 주소 반환
 void *mem_heap_lo()
 {
     return (void *)mem_start_brk;
@@ -79,6 +97,7 @@ void *mem_heap_lo()
 /* 
  * mem_heap_hi - return address of last heap byte
  */
+//힙의 마지막 주소 반환
 void *mem_heap_hi()
 {
     return (void *)(mem_brk - 1);
@@ -87,6 +106,7 @@ void *mem_heap_hi()
 /*
  * mem_heapsize() - returns the heap size in bytes
  */
+//힙의 사이즈를 바이트로 반환
 size_t mem_heapsize() 
 {
     return (size_t)(mem_brk - mem_start_brk);
@@ -95,6 +115,7 @@ size_t mem_heapsize()
 /*
  * mem_pagesize() - returns the page size of the system
  */
+//시스템의 페이지 사이즈를 반환
 size_t mem_pagesize()
 {
     return (size_t)getpagesize();
--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -38,16 +38,67 @@ team_t team = {
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
+//size보다 큰 가장 가까운 ALIGNMENT의 배수로 만들어준다 => 정렬
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7) 
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+//메모리 할당 시 기본적으로 header와 footer를 위해 필요한 더블워드만큼의 메모리 크기
+//size_t: 해당 시스템에서 어떤 객체나 값이 포함할 수 있는 최대 크기의 데이터 
+//32비트 시스템이면 4바이트
+//64바이트면 8바이트
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t))) //
+
+//가용 리스트 메크로 정리
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1<<12)
+
+#define MAX(x,y) ((X) > (y) ? (x) : (y))
+#define PACK(size, alloc) ( (size) | (alloc) )
+
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p,val) (*(unsigned int *)(p)) = val
+
+#define GET_SIZE(p)  (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FDRP(bp) ((char *)(bp) - GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE((char *)(bp) - WSIZE))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))
+
+static char *heap_listp;
 
 /*
- * mm_init - initialize the malloc package.
+ * mm_init  할당기 초기화 하는 함수 성곡하면 0 아니면 -1리턴
  */
 int mm_init(void)
 {
+    //(1) 메모리 시스템에서 4워드(16바이트)를 가져와서, 빈 가용리스트를 만듬
+
+    heap_listp = (char *)mem_sbrk(4 * WSIZE);  // 할당된 메모리의 시작주소
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) return -1;
+
+    //(2) 16바이트를 채워야 함
+    PUT(heap_listp, 0); // 
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE,1)); // 프롤로그 헤더
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE,1)); // 프롤로그 푸터
+    PUT(heap_listp + (3 * WSIZE), PACK(DSIZE,1)); // 에필로그 헤더
+
+    //(3) 가용블록 생성
+    heap_listp += (2*WSIZE);// 프롤로그 블록의 헤더 직후 위치
+
+    //(4) 힙 확장하기(CHUNKSIZE : 4096바이트)
+    if(extend_heap(CHUNKSIZE/WSIZE) == NULL)return -1;
+    
     return 0;
+}
+// 힙을 초기화 했을때
+// 메모리 할당 요청을 받았는데 빈블록을 못 찾았을때
+// 힙 메모리 추가요청하는 함수
+void *extend_heap(size_t words){ 
+    size_t size = ALIGN(words * WSIZE);
+
 }
 
 /*
@@ -68,7 +119,7 @@ void *mm_malloc(size_t size)
 }
 
 /*
- * mm_free - Freeing a block does nothing.
+ * mm_free - ptr이 가리키는 할당된 메모리 블록 해제
  */
 void mm_free(void *ptr)
 {
@@ -92,4 +143,17 @@ void *mm_realloc(void *ptr, size_t size)
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
+}
+
+static void *extend_heap(size_t words){
+
+}
+static void *coalesce(void *bp){
+
+}
+static void *find_fit(size_t asize){
+
+}
+static void place(void *bp, size_t newsize){
+
 }
